41588b68765badbfa4456b06eba8417772dd982c
Eli Popov <eli.popov96@gmail.com>
2023-12-03 18:35:29 +0200

    added util functions

---
+++ backend/utils.ts
@@ -0,0 +1,23 @@
+import { point, buffer, randomPosition, bbox, distance } from '@turf/turf';
+
+export type Position = { lat: number; lng: number };
+
+export function generateRandom(center: Position, radius: number): Position {
+  const ball = point([center.lat, center.lng]);
+  const dist = buffer(ball, radius);
+  const randomPos = randomPosition(bbox(dist));
+  const goal: Position = { lat: randomPos[0], lng: randomPos[1] };
+  return goal;
+}
+
+export function checkGoal(
+  ball: Position,
+  goalDistance: number,
+  goal: Position
+) {
+  const ballPosition = point([ball.lat, ball.lng]);
+  const goalPosition = point([goal.lat, goal.lng]);
+  const dist = distance(ballPosition, goalPosition);
+
+  return goalDistance < dist;
+}
