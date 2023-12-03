a7f60dc4553d29d11e73a42f79d5a98a371eb747
Eli Popov <eli.popov96@gmail.com>
2023-12-03 18:53:06 +0200

    added routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,34 @@
-import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
-
-dotenv.config();
+import { generateRandomGoal, checkGoal, Position } from './utils';
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World From the Typescript Server!');
+app.get('/generateGoal', (req, res) => {
+  const { latitude, longitude, radius } = req.body;
+  const position: Position = { lat: latitude, lng: longitude };
+  const pos = generateRandomGoal(position, radius);
+
+  res.status(200).send({ pos });
 });
 
+app.get('/doesGoalOccurs', (req, res) => {
+  const {
+    ballLatitude,
+    ballLongitude,
+    goalLatitude,
+    goalLongitude,
+    goalDistance,
+  } = req.body;
+  const ballPos: Position = { lat: ballLatitude, lng: ballLongitude };
+  const goallPos: Position = { lat: goalLatitude, lng: goalLongitude };
+  const resBool = checkGoal(ballPos, goallPos, goalDistance);
+
+  res.status(200).send({ resBool });
+});
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
